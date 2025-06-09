1cfd9ce74bc41387f6dadb0226155af4b086f20a
Oscar Marin <oscar.marin@dailybot.com>
2025-06-08 20:21:21 -0500

    404

--- script.js
+++
@@ -1,6 +0,0 @@
-function playSound(bodyPart) {
-    const audio = new Audio(`Media/${bodyPart}.mp3`);
-    audio.play().catch(error => {
-        console.error('Error playing audio:', error);
-    });
-}
